0be765b2b302611c0b379139fa53469404fa2c98
Tomi Harju <harjuto@gmail.com>
2017-05-20 09:33:27 +0300

    added missing files

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import './App.css';
+
+export interface HelloProps { compiler: string; framework: string; }
+
+class App extends React.Component<HelloProps, undefined> {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
